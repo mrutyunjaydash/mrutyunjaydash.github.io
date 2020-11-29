16fe45d1e3eee110c20eb54324a8166a6d0ba869
mrutyunjaydash <mdash555@gmail.com>
2020-11-29 21:51:39 +0530

    experience page modified

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -4,25 +4,11 @@
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
-  },
-  {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
-  },
-  {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Frontend Developer Intern",
+    cardImage: "assets/images/experience-page/marketgad.png",
+    place: "MarketGad",
+    time: "(July, 2020 - present)",
+    desp: "<li>Worked in making the Frontend UI of the website</li><li>Used React for making context hooks and linking APIs</li><li>Connected different routes and added many features to their website</li>",
   },
 ];
 
@@ -114,50 +100,3 @@ const showCards = () => {
   volunteership.innerHTML = output;
 };
 document.addEventListener("DOMContentLoaded", showCards);
-
-
-// Mentorship Card
-
-
-const mentorshipcards = document.querySelector(".mentorship-cards");
-const mentor = [
-  {
-    title: "HakinCode",
-    image: "assets/images/experience-page/hakin.png",
-    time: "06/2020 - 08/2020",
-    desp: "<li>It is an open source community where students and mentors can apply.</li><hr /><li>Ample amount of technologies and projects are there and we are given opportunity to work on them according to our interest and knowledge.</li>",
-  },
-  {
-    title: "Google Summer of Code",
-    image: "assets/images/experience-page/gsoc.png",
-    time: "03/2020 - 08/2020",
-    desp: "<li>Google Summer of Code is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
-  },
-];
-
-const showCards3 = () => {
-  let output = "";
-  mentor.forEach(
-    ({ title, image, time, desp}) =>
-      (output += `        
-      <div class="column mentorshipCard"> 
-      <div class="card card2">
-        <img src="${image}" class="card-img-top" alt="..."  width="64" height="300">
-        <div class="information">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class=""sub-title">${time}</p>
-        </div>
-        <div class="more-information">
-        <ul class="list-group list-group-flush p-0 right-aligned">
-          <div class="list-group-item card2 disclaimer">${desp}</div>
-        </ul>
-        </div>
-        </div>
-      </div>
-      </div>
-      `)
-  );
-  mentorshipcards.innerHTML = output;
-};
-document.addEventListener("DOMContentLoaded", showCards3);
