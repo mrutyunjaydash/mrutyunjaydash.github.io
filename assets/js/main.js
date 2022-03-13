067ac0fa5ec187d578597611718a87ab754c4ac1
Mrutyunjay Dash <mdash555@gmail.com>
2022-03-13 19:04:44 +0530

    resume link updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ let header = $(`
         <li><a class="white-text" href="experience.html">Experience</a></li>
         <li><a class="white-text" href="projects.html">Projects</a></li>
         <li><a class="white-text" href="education.html">Education</a></li>
-        <li><a class="white-text" href="https://drive.google.com/file/d/1k_g0K_MfpYgPLoHaCR8uSsBocVBHKw-N/view?usp=sharing" target="_blank">My Resume</a></li>
+        <li><a class="white-text" href="https://drive.google.com/file/d/1ka6537PoEhrkpkTaFvdRB-IzpcHM_BwW/view?usp=sharing" target="_blank">My Resume</a></li>
         <!--TOGGLE FOR LIGHT/DARK MODE-->
         <li>
         <label for="switch" class="theme-switch">
@@ -62,7 +62,7 @@ let header = $(`
 <li><a href="experience.html">Experience</a></li>
 <li><a href="projects.html">Projects</a></li>
 <li><a href="education.html">Education</a></li>
-<li><a href="https://drive.google.com/file/d/1k_g0K_MfpYgPLoHaCR8uSsBocVBHKw-N/view?usp=sharing" target="_blank">My Resume</a></li>
+<li><a href="https://drive.google.com/file/d/1ka6537PoEhrkpkTaFvdRB-IzpcHM_BwW/view?usp=sharing" target="_blank">My Resume</a></li>
 
 
 <!--TOGGLE FOR LIGHT/DARK MODE-->
