375a904b9857bee9443feb5e0d3a964f509a8ca2
Mrutyunjay Dash <mdash555@gmail.com>
2023-10-12 21:04:50 +0530

    resume link updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ let header = $(`
         <li><a class="white-text" href="experience.html">Experience</a></li>
         <li><a class="white-text" href="projects.html">Projects</a></li>
         <li><a class="white-text" href="education.html">Education</a></li>
-        <li><a class="white-text" href="https://drive.google.com/file/d/1PCh0jbF0Ste8tV6F-1J4zOdvtvtfHy6R/view?usp=drive_link" target="_blank">My Resume</a></li>
+        <li><a class="white-text" href="https://bit.ly/mrutyunjay_dash" target="_blank">My Resume</a></li>
         <!--TOGGLE FOR LIGHT/DARK MODE-->
         <li>
         <label for="switch" class="theme-switch">
@@ -62,7 +62,7 @@ let header = $(`
 <li><a href="experience.html">Experience</a></li>
 <li><a href="projects.html">Projects</a></li>
 <li><a href="education.html">Education</a></li>
-<li><a href="https://drive.google.com/file/d/1PCh0jbF0Ste8tV6F-1J4zOdvtvtfHy6R/view?usp=drive_link" target="_blank">My Resume</a></li>
+<li><a href="https://bit.ly/mrutyunjay_dash" target="_blank">My Resume</a></li>
 
 
 <!--TOGGLE FOR LIGHT/DARK MODE-->
